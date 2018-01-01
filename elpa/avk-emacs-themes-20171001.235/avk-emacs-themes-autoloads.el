61245de3daa1587fb05b346fb8306781407d910e
aritra-b-11 <aritrabhattacharjee12@gmail.com>
2018-01-01 23:26:03 +0530

    python

---
+++ elpa/avk-emacs-themes-20171001.235/avk-emacs-themes-autoloads.el
@@ -0,0 +1,40 @@
+;;; avk-emacs-themes-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "avk-darkblue-white-theme" "avk-darkblue-white-theme.el"
+;;;;;;  (23033 59678 689924 675000))
+;;; Generated autoloads from avk-darkblue-white-theme.el
+
+(when (and load-file-name (boundp 'custom-theme-load-path)) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil "avk-darkblue-yellow-theme" "avk-darkblue-yellow-theme.el"
+;;;;;;  (23033 59678 745926 54000))
+;;; Generated autoloads from avk-darkblue-yellow-theme.el
+
+(when (and load-file-name (boundp 'custom-theme-load-path)) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil "avk-daylight-theme" "avk-daylight-theme.el"
+;;;;;;  (23033 59678 801927 431000))
+;;; Generated autoloads from avk-daylight-theme.el
+
+(when (and load-file-name (boundp 'custom-theme-load-path)) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil nil ("avk-emacs-themes-pkg.el" "avk-emacs-themes.el")
+;;;;;;  (23033 59678 857928 810000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; avk-emacs-themes-autoloads.el ends here
---
+++ elpa/btc-ticker-20151113.659/btc-ticker-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "btc-ticker" "20151113.659" "Shows latest bitcoin price" '((json "1.2") (request "0.2.0")) :commit "845235b545f070d0812cd1654cbaa4997565824f" :keywords '("news"))
